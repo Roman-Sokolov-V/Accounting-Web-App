f174fb56cae5c6e8f0ca5136f2073888f7f79a87
Roman Sokolov <gnonasis@gmail.com>
2026-04-22 11:11:27 +0300

    add context manager for session

--- db/engine.py
+++ db/engine.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
@@ -8,4 +9,16 @@ SQLALCHEMY_DATABASE_URL = f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
+
+@contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # передаємо сесію в код
+        db.commit()
+    except Exception as e:
+        db.rollback() # відкат при помилці
+        raise Exception(e)
+    finally:
+        db.close() # ГАРАНТОВАНЕ закриття
