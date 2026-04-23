db7cdc151c69b7c6b5d9d00f3aa3b27f8450257d
Roman Sokolov <gnonasis@gmail.com>
2026-04-23 12:28:41 +0300

    add  get_partner, get_partner_enries, get_all_transactions get_all_entries

--- db/crud.py
+++ db/crud.py
@@ -1,8 +1,9 @@
+from datetime import datetime, date, timedelta
 from decimal import Decimal
 
 from sqlalchemy import func
 from sqlalchemy.sql.functions import coalesce
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload, selectinload
 
 from db.engine import SessionLocal, engine, get_db
 from db.models import (
@@ -22,14 +23,48 @@ def create_partner(db: Session, name: str, description: str|None=None):
 def get_partners(db: Session):
     return db.query(DBPartners).all()
 
+def get_partner(db: Session, partner_id: int|None = None):
+    if partner_id is not None:
+        partner = db.query(DBPartners).filter(
+            DBPartners.id == partner_id
+        ).first()
+        return partner
+    else:
+        raise Exception('No partner_id provided')
+
+def get_partner_entries(db: Session, partner_id: int|None = None):
+    if partner_id is not None:
+        entries = (
+            db.query(DBEntries)
+            .join(DBEntries.transaction)
+            .join(DBEntries.account)
+            .filter(
+                DBTransactions.partner_id == partner_id,
+                DBAccounts.code.in_([1100, 2000])
+            )
+            .options(
+                joinedload(DBEntries.account),
+                joinedload(DBEntries.transaction)
+            )
+            .order_by(DBEntries.date.asc())
+            .all()
+        )
+        return entries
+    else:
+        raise Exception('No partner_id provided')
+
+
+
 def create_transaction(
         db: Session,
+        date: datetime,
         type: str,
         amount: Decimal,
         partner_id: int,
         description: str|None=None
 ):
     transaction = DBTransactions(
+        date=date,
         type=type,
         amount=amount,
         partner_id=partner_id,
@@ -37,6 +72,11 @@ def create_transaction(
     )
     db.add(transaction)
     return transaction
+
+def get_all_transactions(db: Session):
+    return db.query(DBTransactions).options(
+        joinedload(DBTransactions.entries)
+    ).all()
 
 def get_account_id(db: Session, name:str|None = None, code: int|None = None):
     if name:
@@ -49,6 +89,7 @@ def get_account_id(db: Session, name:str|None = None, code: int|None = None):
 
 def create_entry(
         db: Session,
+        date: datetime,
         transaction_id: int,
         account_id: int,
         debit: Decimal|None = None,
@@ -56,6 +97,7 @@ def create_entry(
 ):
     entry = DBEntries(
         transaction_id=transaction_id,
+        date=date,
         account_id=account_id,
         debit=debit,
         credit=credit
@@ -63,6 +105,10 @@ def create_entry(
     db.add(entry)
     return entry
 
+def get_all_entries(db: Session):
+    return db.query(DBEntries).options(
+        joinedload(DBEntries.account)
+    ).all()
 
 
 def get_account_by_name(db: Session, name: str):
@@ -70,6 +116,7 @@ def get_account_by_name(db: Session, name: str):
 
 def create_transaction_and_entries(
         db:Session,
+        date: datetime,
         type: TransactionsTypes,
         amount: Decimal,
         partner_id: int,
@@ -89,7 +136,7 @@ def create_transaction_and_entries(
         # + (Збільшення) = Credit
         # - (Зменшення) = Debit
         transaction = create_transaction(
-            db, type, amount, partner_id, description
+            db, date, type, amount, partner_id, description
         )
         db.flush()  # Отримуємо ID транзакції
         # 2. Логіка вибору рахунків (Accounting Logic)
@@ -123,7 +170,7 @@ def create_transaction_and_entries(
 
         # 3. Створюємо записи (Entries)
         for entry_data in entries:
-            create_entry(db=db, transaction_id=transaction.id, **entry_data)
+            create_entry(db=db, transaction_id=transaction.id, date=transaction.date, **entry_data)
         return transaction
     except Exception as e:
         db.rollback()  # Скасовуємо ВСЕ, якщо хоча б один крок не вдався
@@ -136,6 +183,27 @@ def get_total_revenue(db: Session):
         func.sum(coalesce(DBEntries.debit, 0))
     ).filter(DBEntries.account_id == revenue_account.id).scalar()
 
+
+def first_last_days_prev_month():
+    today = date.today()
+    first_day_current = today.replace(day=1)
+    last_day_prev_month = first_day_current - timedelta(days=1)
+    first_day_prev_month = last_day_prev_month.replace(day=1)
+    return first_day_prev_month, last_day_prev_month
+
+def get_total_revenue_prev_month(db: Session):
+    revenue_account = get_account_by_name(db, "Revenue")
+    first_day_last_month, last_day_last_month = first_last_days_prev_month()
+
+    return db.query(
+        func.sum(coalesce(DBEntries.credit, 0)) -
+        func.sum(coalesce(DBEntries.debit, 0))
+    ).filter(
+        DBEntries.account_id == revenue_account.id,
+        DBEntries.date >= first_day_last_month,
+        DBEntries.date <= last_day_last_month
+    ).scalar()
+
 def get_total_expense(db: Session):
     expense_account = get_account_by_name(db, "Expense")
     return db.query(
@@ -143,12 +211,35 @@ def get_total_expense(db: Session):
         func.sum(coalesce(DBEntries.credit, 0))
     ).filter(DBEntries.account_id == expense_account.id).scalar()
 
+def get_total_expense_prev_month(db: Session):
+    expense_account = get_account_by_name(db, "Expense")
+    first_day_last_month, last_day_last_month = first_last_days_prev_month()
+    return db.query(
+        func.sum(coalesce(DBEntries.debit, 0)) -
+        func.sum(coalesce(DBEntries.credit, 0))
+    ).filter(
+        DBEntries.account_id == expense_account.id,
+        DBEntries.date >= first_day_last_month,
+        DBEntries.date <= last_day_last_month
+    ).scalar()
+
 def get_total_cash(db: Session):
     cash_account = get_account_by_name(db, "Cash")
     return db.query(
         func.sum(coalesce(DBEntries.debit, 0)) -
         func.sum(coalesce(DBEntries.credit, 0))
     ).filter(DBEntries.account_id == cash_account.id).scalar()
+
+def get_total_cash_prev_month(db: Session):
+    cash_account = get_account_by_name(db, "Cash")
+    _, last_day_last_month = first_last_days_prev_month()
+    return db.query(
+        func.sum(coalesce(DBEntries.debit, 0)) -
+        func.sum(coalesce(DBEntries.credit, 0))
+    ).filter(
+        DBEntries.account_id == cash_account.id,
+        DBEntries.date <= last_day_last_month
+    ).scalar()
 
 if __name__ == "__main__":
     with get_db() as db:
