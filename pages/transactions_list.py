63046510447f3e601d5b59971dd479806d229797
Roman Sokolov <gnonasis@gmail.com>
2026-04-23 12:34:30 +0300

    add sidebar to transactions_list.py

--- pages/transactions_list.py
+++ pages/transactions_list.py
@@ -2,7 +2,12 @@ import streamlit as st
 from db.crud import get_all_transactions
 from db.engine import get_db
 
-
+with st.sidebar:
+    st.page_link("pages/transactions.py", label="Add transaction", icon="➕")
+    st.page_link("pages/profit_loss.py", label="Profit & Loss", icon="📊")
+    st.page_link("pages/partner_ledger.py", label="Partner ledger", icon="👥")
+    st.page_link("pages/transactions_list.py", label="Transactions list", icon="📋")
+    st.page_link("pages/entries_list.py", label="Entries list")
 
 with get_db() as session:
     transactions = get_all_transactions(db=session)
