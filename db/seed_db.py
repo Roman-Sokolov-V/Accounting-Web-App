7209ca6c33286b1970c7a6d6204e011740c0ca97
Roman Sokolov <gnonasis@gmail.com>
2026-04-24 09:53:10 +0300

    refactoring seed_db.py - support postgres

--- db/seed_db.py
+++ db/seed_db.py
@@ -1,38 +1,38 @@
-from datetime import date, timedelta
-
 import random
+from datetime import date, timedelta
 from decimal import Decimal
 
-from db.crud import get_partners, create_transaction_and_entries, get_accounts
-from db.engine import SessionLocal, get_db
-from db.models import DBAccounts, DBPartners, DBTransactions, TransactionsTypes
+from db.crud import create_transaction_and_entries, get_partners
+from db.engine import get_db
+from db.models import DBAccounts, DBPartners, TransactionsTypes
 
 
-def seed_accounts(session=SessionLocal()):
-    existing = session.query(DBAccounts).first()
-    if existing:
-        return
+def seed_accounts(db=get_db()):
+    with db as session:
+        existing = session.query(DBAccounts).first()
+        if existing:
+            return
 
-    accounts = [
-        DBAccounts(code=1000, name="Cash", type="asset"),
-        DBAccounts(code=1100, name="Accounts Receivable", type="asset"),
-        DBAccounts(code=2000, name="Accounts Payable", type="liability"),
-        DBAccounts(code=4000, name="Revenue", type="income"),
-        DBAccounts(code=5000, name="Expense", type="expense"),
-    ]
+        accounts = [
+            DBAccounts(code=1000, name="Cash", type="asset"),
+            DBAccounts(code=1100, name="Accounts Receivable", type="asset"),
+            DBAccounts(code=2000, name="Accounts Payable", type="liability"),
+            DBAccounts(code=4000, name="Revenue", type="income"),
+            DBAccounts(code=5000, name="Expense", type="expense"),
+        ]
 
-    session.add_all(accounts)
-    session.commit()
-    session.close()
+        session.add_all(accounts)
+        session.commit()
 
-def seed_partners(session=SessionLocal()):
-    existing = session.query(DBPartners).first()
-    if existing:
-        return
-    partners = [DBPartners(name=f"partner {i}") for i in range(1, 10)]
-    session.add_all(partners)
-    session.commit()
-    session.close()
+
+def seed_partners(db=get_db()):
+    with db as session:
+        existing = session.query(DBPartners).first()
+        if existing:
+            return
+        partners = [DBPartners(name=f"partner {i}") for i in range(1, 10)]
+        session.add_all(partners)
+        session.commit()
 
 
 def seed_transactions(db=get_db()):
@@ -44,7 +44,6 @@ def seed_transactions(db=get_db()):
 
         partners_ids = [partner.id for partner in partners]
 
-        # ВИПРАВЛЕННЯ: беремо типи транзакцій з Enum, а не з акаунтів
         transaction_types = list(TransactionsTypes)
 
         today_val = date.today()
@@ -58,15 +57,14 @@ def seed_transactions(db=get_db()):
                         date=d,
                         type=random.choice(transaction_types),
                         amount=Decimal(random.randint(100, 10000)),
-                        partner_id=random.choice(partners_ids)
+                        partner_id=random.choice(partners_ids),
                     )
                 except Exception as e:
                     print(f"Помилка при створенні: {e}")
         session.commit()
 
 
-
-if __name__ == '__main__':
-    # seed_accounts()
-    # seed_partners()
+if __name__ == "__main__":
+    seed_accounts()
+    seed_partners()
     seed_transactions()
