1352d5c911f473f9fd3c2de51b8e9ba606e73b1d
Roman Sokolov <gnonasis@gmail.com>
2026-04-24 09:50:59 +0300

    add postgres settings in alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -1,15 +1,17 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
+import settings
 from alembic import context
-
 from db.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+db_url = settings.SQLALCHEMY_DATABASE_URL
+config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
